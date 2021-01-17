b888b1cdf781eee811d85d4e29abefaf0d75e7a7
Javier Martin <81javier@gmail.com>
2021-01-17 14:03:09 -0300

    agregue items a shp

--- scripts/repuestos.js
+++ scripts/repuestos.js
@@ -2,38 +2,193 @@
 
 
 
-   const stockRepuestos = [
+  /* const products = [
         
 
     
-      {"Distribucion-rhy" : {
-            
+      {
+            "id":"00001",
             "modelo":"Peugeot 307",
             "motor":"rhy",
-            "codigo":"00001",
-            "tipo":"kit de distribucion",
+            "producto":"kit de distribucion",
             "descripcion":"Kit de distribucion origuinal con bomba de agua",
-            "precio":"$7800"
-       }},
+            "precio":"$7800",
+            "available":true
+       },
         
-         {"Distribucion-ew10":{  
+         {  "id":"00002",
             "modelo":"Peugeot 307",
             "motor":"ew10",
-            "codigo":"00002",
-            "tipo":"kit de distribucion",
+            "producto":"kit de distribucion",
             "descripcion":"Kit de distribucion origuinal con bomba de agua",
             "precio":"$11800",
-            "imagen": src="media/imggaleria/distribucion_408.jpg"
-        }}
+            "imagen": src="media/imggaleria/distribucion_408.jpg",
+            "available":true
+        }
 
     
 
     
 
       
-    ];
+    ];*/
 
-    
+    const products = [{
+        "id" : "0001",
+        "name" : "Distribucion 2.0 ew10",
+        "description" : "kit de distribucion con bomba de agua para motor 2.0 16v de peugeot y citroen.",
+        "img" : "media/shop/dist40820.png",
+        "price" : 13200,
+        "available" : true,
+        tags : ["ew10", "peugeot", "repuestos"]
+      },{
+        "id" : "0002",
+        "name" : "Distribucion 2.0 hdi",
+        "description" : "kit de distribucion con bomba de agua para motor 2.0 hdi de peugeot y citroen.",
+        "img" : "media/shop/dist2.0hdi.png",
+        "price" : 9800,
+        "available" : true,
+        tags : ["phone", "apple"]
+      },{
+        "id" : "0003",
+        "name" : "Iphone",
+        "description" : "kit de distribucion con bomba de agua para motor 1.6 hdi de peugeot y citroen.",
+        "img" : "media/shop/distdv6.png",
+        "price" : 12500,
+        "available" : true,
+        tags : ["phone", "apple"]
+      },{
+        "id" : "0004",
+        "name" : "Iphone",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit tempus hendrerit.",
+        "img" : "https://picsum.photos/300/200",
+        "price" : 200,
+        "available" : true,
+        tags : ["phone", "apple"]
+      },{
+        "id" : "0005",
+        "name" : "Iphone",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit tempus hendrerit.",
+        "img" : "https://picsum.photos/300/200",
+        "price" : 200,
+        "available" : true,
+        tags : ["phone", "apple"]
+      },{
+        "id" : "0006",
+        "name" : "Iphone",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit tempus hendrerit.",
+        "img" : "https://picsum.photos/300/200",
+        "price" : 200,
+        "available" : true,
+        tags : ["phone", "apple"]
+      },{
+        "id" : "0007",
+        "name" : "Iphone",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit tempus hendrerit.",
+        "img" : "https://picsum.photos/300/200",
+        "price" : 200,
+        "available" : true,
+        tags : ["phone", "apple"]
+      },{
+        "id" : "0008",
+        "name" : "Iphone",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit tempus hendrerit.",
+        "img" : "https://picsum.photos/300/200",
+        "price" : 200,
+        "available" : true,
+        tags : ["phone", "apple"]
+      },{
+        "id" : "0009",
+        "name" : "Iphone",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit tempus hendrerit.",
+        "img" : "https://picsum.photos/300/200",
+        "price" : 200,
+        "available" : true,
+        tags : ["phone", "apple"]
+      },{
+        "id" : "0010",
+        "name" : "Iphone",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit tempus hendrerit.",
+        "img" : "https://picsum.photos/300/200",
+        "price" : 200,
+        "available" : true,
+        tags : ["phone", "apple"]
+      },{
+        "id" : "0011",
+        "name" : "Iphone",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit tempus hendrerit.",
+        "img" : "https://picsum.photos/300/200",
+        "price" : 200,
+        "available" : true,
+        tags : ["phone", "apple"]
+      },{
+        "id" : "0012",
+        "name" : "Iphone",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit tempus hendrerit.",
+        "img" : "https://picsum.photos/300/200",
+        "price" : 200,
+        "available" : true,
+        tags : ["phone", "apple"]
+      },{
+        "id" : "0013",
+        "name" : "Iphone",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit tempus hendrerit.",
+        "img" : "https://picsum.photos/300/200",
+        "price" : 200,
+        "available" : true,
+        tags : ["phone", "apple"]
+      },{
+        "id" : "0015",
+        "name" : "Iphone",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit tempus hendrerit.",
+        "img" : "https://picsum.photos/300/200",
+        "price" : 200,
+        "available" : true,
+        tags : ["phone", "apple"]
+      },{
+        "id" : "0016",
+        "name" : "Iphone",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit tempus hendrerit.",
+        "img" : "https://picsum.photos/300/200",
+        "price" : 200,
+        "available" : true,
+        tags : ["phone", "apple"]
+      },{
+        "id" : "0017",
+        "name" : "Iphone",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit tempus hendrerit.",
+        "img" : "https://picsum.photos/300/200",
+        "price" : 200,
+        "available" : true,
+        tags : ["phone", "apple"]
+      },{
+        "id" : "0017",
+        "name" : "Iphone",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit tempus hendrerit.",
+        "img" : "https://picsum.photos/300/200",
+        "price" : 200,
+        "available" : true,
+        tags : ["phone", "apple"]
+      },
+
+      {
+        "id" : "0018",
+        "name" : "Chromecast",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit tempus hendrerit.",
+        "img" : "https://picsum.photos/300/200",
+        "price" : 300,
+        "available" : false,
+        tags : ["tv", "google"]
+      },{
+        "id" : "0019",
+        "name" : "TV",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit tempus hendrerit.",
+        "img" : "https://picsum.photos/300/200",
+        "price" : 400,
+        "available" : true,
+        tags : ["tv", "google"]
+      }
+      ];
 
 
 
@@ -42,17 +197,7 @@
   
 
 
-let data1 = document.getElementById('code-00001_Descripcion1');
-let data2 = document.getElementById('code-00001_Descripcion2');
-let data3 = document.getElementById('code-00001_precio');
-let data4 = document.getElementById('code-00001_img');
 
-
-
-data1.innerHTML=stockRepuestos[1]["Distribucion-ew10"].descripcion;
-data2.innerHTML=stockRepuestos[1]["Distribucion-ew10"].modelo;
-data3.innerHTML=stockRepuestos[1]["Distribucion-ew10"].precio;
-data4.innerHTML=stockRepuestos[1]["Distribucion-ew10"].imagen;
 
 
 
